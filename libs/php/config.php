6798b5eeda6b7f88099eb76146ee696baef2ba8e
Haroon Khan <HaroonKhan120704@gmail.com>
2025-04-01 12:40:47 +0100

    fixed config

--- libs/php/config.php
+++ libs/php/config.php
@@ -3,68 +3,23 @@
 
 	// connection details for MySQL database
 	// development
-	// $cd_host = "127.0.0.1";
-	// $cd_port = 3306;
-	// $cd_socket = "";
+	$cd_port = 3306;
+	$cd_socket = "";
 
-	// // database name, username and password
+	// database name, username and password
 
-	// $cd_dbname = "companydirectory";
-	// $cd_user = "root";
-	// $cd_password = "";
+	$cd_dbname = "harofbou_companydirectory";
+	$cd_user = "harofbou_test";
+	$cd_password = "RmD!=?T}&Zgw";
+    $cd_host = "server242";
+    $conn = mysqli_connect($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+      if(!$conn){
+        echo ' Not successfull';
+        die('Connect failed!!'.mysqli_connect_error());
 
+    } else {
+        echo 'Connect successful';
+    }
 
-
-// Production dont worry about code above
-
-	// $host = "svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com";
-	// $port = 3333;
-	// $dbname = "db_haroon_5c8d4";
-	// $username = "haroon-f667e";
-	// $password = "kbuzdODaQVS1YkdnuZEcm28wJkd0lcB7";
-	// $host = "svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com";
-	// $port = 3333;
-	// $user = "haroon-f667e";
-	// $password = "kbuzdODaQVS1YkdnuZEcm28wJkd0lcB7";
-	// $db = "db_haroon_5c8d4";
-	
-	// // Path to SSL certificate
-	// $ssl_ca = __DIR__ . "/singlestore_bundle.pem";
-	
-	// // Create MySQLi connection
-	// $mysqli = mysqli_init();
-	
-	// // Set the SSL parameters
-	// $mysqli->ssl_set(null, null, $ssl_ca, null, null);
-	
-	// // Attempt to connect to the database
-	// if ($mysqli->real_connect($host, $user, $password, $db, $port)) {
-	// 	echo "✅ Successfully connected to the database!";
-	// } else {
-	// 	die("❌ MySQLi Connection Error: " . $mysqli->connect_error);
-	// }
-	
-	// $mysqli->close();
-	$host = "svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com";
-	$port = 3333;
-	$user = "haroon-f667e";
-	$password = "kbuzdODaQVS1YkdnuZEcm28wJkd0lcB7";
-	$db = "db_haroon_5c8d4";
-	$ssl_ca = realpath(__DIR__ . "/../../singlestore_bundle.pem");
-
-
-	try {
-
-	    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
-	
-	    $pdo = new PDO($dsn, $user, $password, [
-	        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-	        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca
-	    ]);
-
-	    echo "✅ Successfully connected to the database!";
-	} catch (PDOException $e) {
-	    echo "❌ Connection failed: " . $e->getMessage();
-	}
 ?>
 
